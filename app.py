9a6ade69aa8e78c55e825be72416713247295c59
Arnav Jain <arnavj1903@gmail.com>
2024-06-04 10:56:42 +0530

    morning

--- app.py
+++ app.py
@@ -301,7 +301,3 @@ def insert_data():
         db.session.commit()
     return 'Data inserted successfully'
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
